20009b6c0cc598e21c4c3fd62bc805fa8dd73997
Yogesh Singh <yogeshpix@gmail.com>
2021-06-30 19:06:01 +0530

    Again

--- src/Hooks/UseRef.js
+++ src/Hooks/UseRef.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 function UseRef() {
-  return <div></div>;
+  let [timer, setTimer] = useState(0);
+  let inputRef = useRef(null);
+  let intervalRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+
+    intervalRef.current = setInterval(() => {
+      setTimer((e) => e + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+  return (
+    <div>
+      <input type='text' ref={inputRef} />
+      <p>Timer - {timer}</p>
+      <button onClick={() => clearInterval(intervalRef.current)}>
+        Stop Timer
+      </button>
+    </div>
+  );
 }
 
 export default UseRef;
